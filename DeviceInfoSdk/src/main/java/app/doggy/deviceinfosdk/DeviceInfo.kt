ddb107e84fb93d3ecfec24f7562f4330fa4c7de3
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-14 19:36:16 +0900

    add: DeviceInfoをSDKにした

---
+++ DeviceInfoSdk/src/main/java/app/doggy/deviceinfosdk/DeviceInfo.kt
@@ -0,0 +1,24 @@
+package app.doggy.deviceinfosdk
+
+import android.os.Build
+
+class DeviceInfo {
+    companion object {
+        // 端末情報を返す．
+        fun getAll(): Map<String, String> {
+            return mapOf(
+                CARRIER to Build.BRAND,
+                DEVICE to Build.DEVICE,
+                MANUFACTURER to Build.MANUFACTURER,
+                MODEL to Build.MODEL,
+                VERSION to Build.VERSION.RELEASE,
+            )
+        }
+
+        const val CARRIER = "BRAND"
+        const val DEVICE = "DEVICE"
+        const val MANUFACTURER = "MANUFACTURER"
+        const val MODEL = "MODEL"
+        const val VERSION = "VERSION_RELEASE"
+    }
+}
