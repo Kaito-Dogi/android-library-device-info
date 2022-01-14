5a83e24172f40939fc6b3811419264c680c39f37
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-14 19:35:58 +0900

    add: MainActivityで端末情報をログ出力

--- app/src/main/java/app/doggy/deviceinfo/MainActivity.kt
+++ app/src/main/java/app/doggy/deviceinfo/MainActivity.kt
@@ -1,11 +1,24 @@
 package app.doggy.deviceinfo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 端末情報をログに書き出す．
+        Log.d(DEVICE_INFO, "キャリア：${Build.BRAND}")
+        Log.d(DEVICE_INFO, "デバイス：${Build.DEVICE}")
+        Log.d(DEVICE_INFO, "製造会社：${Build.MANUFACTURER}")
+        Log.d(DEVICE_INFO, "モデル：${Build.MODEL}")
+        Log.d(DEVICE_INFO, "OSバージョン：${Build.VERSION.RELEASE}")
+    }
+
+    companion object {
+        private const val DEVICE_INFO = "DEVICE_INFO"
     }
 }
