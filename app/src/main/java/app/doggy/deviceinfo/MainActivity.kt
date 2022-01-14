4f01a3e1bd86f39826e2aead5d84de2efbc9bcc5
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-14 20:22:51 +0900

    add: MainActivity内でライブラリモジュールを使用

--- app/src/main/java/app/doggy/deviceinfo/MainActivity.kt
+++ app/src/main/java/app/doggy/deviceinfo/MainActivity.kt
@@ -4,18 +4,21 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import app.doggy.deviceinfosdk.DeviceInfo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val deviceInfo = DeviceInfo.getAll()
+
         // 端末情報をログに書き出す．
-        Log.d(DEVICE_INFO, "キャリア：${Build.BRAND}")
-        Log.d(DEVICE_INFO, "デバイス：${Build.DEVICE}")
-        Log.d(DEVICE_INFO, "製造会社：${Build.MANUFACTURER}")
-        Log.d(DEVICE_INFO, "モデル：${Build.MODEL}")
-        Log.d(DEVICE_INFO, "OSバージョン：${Build.VERSION.RELEASE}")
+        Log.d(DEVICE_INFO, "キャリア：${deviceInfo[DeviceInfo.CARRIER]}")
+        Log.d(DEVICE_INFO, "デバイス：${deviceInfo[DeviceInfo.DEVICE]}")
+        Log.d(DEVICE_INFO, "製造会社：${deviceInfo[DeviceInfo.MANUFACTURER]}")
+        Log.d(DEVICE_INFO, "モデル：${deviceInfo[DeviceInfo.MODEL]}")
+        Log.d(DEVICE_INFO, "OSバージョン：${deviceInfo[DeviceInfo.VERSION]}")
     }
 
     companion object {
